2c33276139c42ccf585556d125c70fbec4eadee5
NVRC <nathaniel.charlebois@gmail.com>
2016-12-07 20:39:41 -0500

    ATM nearing completion

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-Program: atm.cpp
+Program: main.cpp
 Authors:  -Benjamin Tobalt
           -Nathaniel Charlebois
 
@@ -22,24 +22,32 @@ using namespace std;
 #include <stdlib.h>
 #include <string>
 #include <iostream>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <exception>
-#include <memory>
-
 #include <pthread.h>
 #include <signal.h>
 #include <sys/time.h>
 #include <sys/stat.h>
 #include <mqueue.h>
 #include <fstream>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <exception>
+#include <memory>
+
+/*
+Couldn't pass complex types like string through the Message queues
+*/
+#define ACC_NUM_SIZE 5
+#define PIN_NUM_SIZE 3
+//Maximum number of messages in the message queue
+#define MAX_MSGS 10;
+
 
 typedef struct general_message_struct{
-  string accountNum;
-  string pin;
+  char accountNum[ACC_NUM_SIZE];
+  char pin[PIN_NUM_SIZE];
 }general_message_struct;
-#define gen_msg_struct_size = sizeof(general_message_struct);
+#define GEN_MSG_STRUCT_SIZE sizeof(general_message_struct);
 
 //Pthread variables
 pthread_t atm_thread;
@@ -52,8 +60,10 @@ pthread_mutex_t server_db_mutex;
 #define ATM_SERVER_NAME "/pin_msg"
 #define SERVER_DB_NAME "/db_msg"
 
+//Initialize static message variables
 static struct mq_attr msgq_attr;
-static mqd_t general_message = -1;
+static mqd_t atm_server_message = -1;
+static mqd_t server_db_message = -1;
 
 void serverPrint(string printInput){
   fstream serverLog;
@@ -71,13 +81,23 @@ void dbPrint(string printInput){
 
 
 void* atm(void* args){
+
+  general_message_struct sendMsg;
+  general_message_struct rcvMsg;
+  unsigned int msg_prio = 0;
+
   int timesAccessed = 0;
   string accountNumber;
   string pin;
+
+  int atmStatus;
+
   //Get account Number
+  cout << "ATM is executing: \n";
   while(accountNumber.size()!=5){
     cout << "Enter Account Number(5 Digits) or 'quit' to exit: ";
     cin >> accountNumber;
+    cout << "You entered: " + accountNumber + "\n";
     if(accountNumber == "quit"){
       cout << "";
       exit(EXIT_FAILURE);
@@ -90,12 +110,56 @@ void* atm(void* args){
   while(pin.size()!=3){
     cout << "Enter PIN: ";
     cin >> pin;
+    cout << "You entered: " + pin + "\n";
     if(pin.size()!=3){
       cout << "Invalid PIN Format \n";
     }
   }
+  //Encode the input to the message packet
+  for(int i=0; i<ACC_NUM_SIZE; i++){
+    sendMsg.accountNum[i] = accountNumber[i];
+  }
+  for(int i=0; i<PIN_NUM_SIZE; i++){
+    sendMsg.pin[i] = pin[i];
+  }
+
+
+  //Send genMsg
+  atmStatus = mq_send(atm_server_message, (const char*) &sendMsg, sizeof(sendMsg), msg_prio);
+  //Check the send status
+  if(atmStatus < 0){
+    cout << "Sending a message via the message queue failed.";
+    exit(0);
+  }
+  //Receive genMsg
+  atmStatus = mq_receive(server_db_message, (char*) &rcvMsg, sizeof(rcvMsg), NULL);
+  //Check the receive status
+  if(atmStatus < 0){
+    cout << "Receiving a message via the message queue failed ";
+    exit(0);
+  }
+
 }
 
-int main(){
+int main(int args, char const *argv[]){
+
+  pthread_attr_t attr;
+
+  //Initialize Message Queue VARS
+  msgq_attr.mq_maxmsg = MAX_MSGS;
+  msgq_attr.mq_msgsize = (size_t) GEN_MSG_STRUCT_SIZE;
+  //Initialize Message Queues
+  atm_server_message = mq_open(ATM_SERVER_NAME, O_CREAT | O_RDWR, 0666, &msgq_attr);
+  server_db_message = mq_open(SERVER_DB_NAME, O_CREAT | O_RDWR, 0666, &msgq_attr);
+  //Check if MSG Queue were initialized
+  if(atm_server_message == -1){
+    cout << "ERROR: ATM<->SERVER message queue failed to Initialize.\n";
+  }
+  if(server_db_message == -1){
+    cout << "ERROR: SERVER<->DB message queue failed to Initialize.\n";
+  }
+
+  //Initialize Threads
+
 
 }
